16145353754fdd9fdc66725f5cd7a61e172e732b
JULINHOGT23 <1485840@senati.pe>
2023-04-15 10:04:46 -0500

    ponemos requerimientos

--- HTML_2/regis2.js
+++ HTML_2/regis2.js
@@ -1,5 +1,11 @@
 function validar() {
-    var nombre, fecha, telefono, password, expresion;
+    var nombre, fecha, telefono, password;
     nombre = document.getElementById("nombre").value;
-    
+    fecha = document.getElementById("fecha").value;
+    telefono = document.getElementById("telefono").value;
+    password = document.getElementById("password").value;
+
+    if(nombre === ""){
+        alert("la ocion nombre esta vasillo")
+    }
 }
